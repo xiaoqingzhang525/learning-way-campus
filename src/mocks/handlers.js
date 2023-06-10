e9e0ca157bd8e8c16f86429a3ce615ef88b9f0b1
Cora Zhang <xiaoqing_zhang525@126.com>
2023-06-10 19:38:53 +0800

    add handler for fetch data

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -4,13 +4,13 @@ import courseAdjustment from './data/courseAdjustment.json';
 import paymentDetail from './data/paymentDetail.json';
 
 export const handlers = [
-  rest.get(/\/orders\/[0-9]+\/payments\/[0-9]+/, async (req, res, ctx) => {
-    return await res(ctx.status(500));
-  }),
+  // rest.get(/\/orders\/[0-9]+\/payments\/[0-9]+/, async (req, res, ctx) => {
+  //   return await res(ctx.status(500));
+  // }),
 
-  rest.get(/\/orders\/[0-9]+\/payments\/[0-9]+/, async (req, res, ctx) => {
-    return await res.networkError('error');
-  }),
+  // rest.get(/\/orders\/[0-9]+\/payments\/[0-9]+/, async (req, res, ctx) => {
+  //   return await res.networkError('error');
+  // }),
 
   rest.get(/\/orders\/[0-9]+\/payments\/[0-9]+/, async (req, res, ctx) => {
     return await res(ctx.status(200), ctx.json({ data: paymentDetail }));
